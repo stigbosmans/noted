8c6980e8bd5ddf4ae751d2e79e9a8906d5a4dc08
sbossman <stigbosmans@gmail.com>
2018-02-07 21:26:48 +0100

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="editor"></div>
+  </div>
+</template>
+
+<script>
+  import TuiEditor from "tui-editor/dist/tui-editor-Editor-all.min"
+  require('tui-editor/dist/tui-editor.min.css')
+
+export default {
+  name: 'Main',
+  data () {
+    return {
+    }
+  },
+  mounted: function () {
+    var editor = new TuiEditor({
+      el: document.querySelector('.editor'),
+      initialEditType: 'wysiwyg',
+      previewStyle: 'vertical',
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .editor {
+    height: 500px;
+  }
+</style>
